0e9ca1c0a0614931c9237464e00669afcad8931e
komal1523 <152951517+komal1523@users.noreply.github.com>
2024-07-24 23:07:20 +0530

    start function
    going into function

---
+++ learn.01/10.Function.js
@@ -0,0 +1,22 @@
+//******************************************  FUNCTION  ******************************************************* */
+
+function sayHello(){   // function definition
+    console.log('K');
+    console.log('o');
+    console.log('m');
+    console.log('a');
+    console.log('l');
+}
+sayHello()        // calling function
+
+// adding two number using function in js
+
+function  addTwoNum(num1, num2){
+ //console.log(num1+num2)
+  return num1+num2
+
+
+}
+console.log(addTwoNum(5, 7));
+
+
--- learn.01/9.object.in.depth.js
+++ learn.01/9.object.in.depth.js
@@ -30,3 +30,26 @@ const obj3={ 5:"a" , 6: "b"}
 //const obj4={...obj1,...obj2,...obj3}
 const obj4  =  Object.assign({},obj1,obj2,obj3)
 console.log(obj4);
+console.log(Object.keys( userInfo));   // to get tg\he keys of oblect userInfo
+console.log(Object.values( userInfo)); // to get the values of object userInfo
+
+
+///********************************************************* Object De-structure *************************************************** */
+
+const course={
+    courseName:"Javascript..Series",
+    price:"1000",
+    courseTeacher: "KumarSingh",
+
+}
+//console.log(course.courseTeacher);   // simple process of accessing value
+
+//const {courseTeacher}=course
+console.log(courseTeacher);
+
+const {courseTeacher:onlineTeacher}=course   // De-structure the values from courseTeacher to onlineTeacher
+console.log(onlineTeacher);
+
+
+
+
